0b6dd59250e1dfb3f34eaa033f9f22d25e9f8a54
Paksh <pakshk2003@gmail.com>
2023-08-18 14:27:37 +0530

    Changed the width of the screen from 100% to 97%.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import ChatPage from './Components/ChatScreen/ChatPage/ChatPage';
+// import MessageBoxHolder from './Components/ChatScreen/MessageBox/MessageBoxHolder/MessageBoxHolder';
+// import SignupPage from './Components/Signup/SignupPage/signupPage';
+// import LoginPage from './Components/Signup/SignupPage/signupPage';
+// import Holder from './Components/ChatScreen/MessagePannel/Holder/Holder';
+// import SideBar from './Components/ChatScreen/SideBar/SideBar';
+// import TypingArea from './Components/ChatScreen/MessageBox/TypingArea/TypingArea';
+// import LoginPage from './Components/ChatScreen/SideBar/LoginPage'
+// import TopInfoPannel from './Components/ChatScreen/MessageBox/TopInfoPannel/TopInfoPannel';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatPage />
     </div>
   );
 }
